0a89eac25298b936090a3b98c8e5f7f1e8972e47
glendc <glen.decauwsemaecker@otainsight.com>
2022-01-22 22:22:05 +0100

    add initial actix-web site

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
-fn main() {
-    println!("Hello, world!");
+use std::collections::HashMap;
+
+use actix_web::{middleware, web, App, HttpResponse, HttpServer, Result};
+use askama::Template;
+
+#[derive(Template)]
+#[template(path = "user.html")]
+struct UserTemplate<'a> {
+    name: &'a str,
+    text: &'a str,
+}
+
+#[derive(Template)]
+#[template(path = "index.html")]
+struct Index;
+
+async fn index(query: web::Query<HashMap<String, String>>) -> Result<HttpResponse> {
+    let s = if let Some(name) = query.get("name") {
+        UserTemplate {
+            name,
+            text: "Welcome!",
+        }
+        .render()
+        .unwrap()
+    } else {
+        Index.render().unwrap()
+    };
+    Ok(HttpResponse::Ok().content_type("text/html").body(s))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "actix_web=info");
+    env_logger::init();
+
+    // start http server
+    HttpServer::new(move || {
+        App::new()
+            .wrap(middleware::Logger::default())
+            .service(web::resource("/").route(web::get().to(index)))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
