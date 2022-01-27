92aa6298c0e9803365e874cf2067615e71d9bb3c
glendc <glen.decauwsemaecker@otainsight.com>
2022-01-27 11:22:29 +0100

    include build semver in footer src/info link

--- src/site/state.rs
+++ src/site/state.rs
@@ -9,6 +9,7 @@ pub struct SiteState {
 impl SiteState {
     pub fn new() -> SiteState {
         let build_timestamp = env!("VERGEN_BUILD_TIMESTAMP");
+        let build_semver = env!("VERGEN_BUILD_SEMVER");
         let build_date = build_timestamp.split("T").next().unwrap();
         let git_sha = env!("VERGEN_GIT_SHA");
         let git_sha_short = git_sha.chars().take(8).collect::<String>();
@@ -19,6 +20,7 @@ impl SiteState {
                 hasher.finish()
             })(),
             build_date,
+            build_semver,
             git_sha,
             git_sha_short,
         };
@@ -29,6 +31,7 @@ impl SiteState {
 pub struct SiteInfo {
     pub version: u64,
     pub build_date: &'static str,
+    pub build_semver: &'static str,
     pub git_sha: &'static str,
     pub git_sha_short: String,
 }
