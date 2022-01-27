5d2606d3f9959ca5c1c4b2f28ce37ef48cd2b990
glendc <glen.decauwsemaecker@otainsight.com>
2022-01-27 11:16:28 +0100

    make js-select (opt) work when js is enabled
    making language selection in header smoother,
    for those who do not mind enabling js
    
    still though, let's keep the JS to the absolute
    minimum and only use it if we really require it for a feature

--- site/assets/client.js
+++ site/assets/client.js
@@ -1,13 +1,18 @@
-(function() {
-    // make drop-down only forms work automatically when
-    // JS is enabled, but also ensure that the site works 100% without
-    // the need for JS
-    
-    const el = document.querySelector("header input.select");
-    if (el) {
-        el.onchange = function() {
-            document.getElementById("site_header_locale_form").submit();
-        };
-        el.style.visibility = "hidden";
-    }
+(function () {
+    document.addEventListener('DOMContentLoaded', function () {
+        // make drop-down only forms work automatically when
+        // JS is enabled, but also ensure that the site works 100% without
+        // the need for JS
+        const form_el = document.querySelector("header form#site_header_locale_form");
+        if (form_el) {
+            const submit_el = form_el.querySelector("input.select");
+            const select_el = form_el.querySelector("select#locale");
+            if (submit_el && select_el) {
+                submit_el.style.visibility = "hidden";
+                select_el.onchange = function () {
+                    form_el.submit();
+                };
+            }
+        }
+    });
 })();
