434d3bee9924608cc5c2f91202a891d4db83fdb1
glendc <glen.decauwsemaecker@otainsight.com>
2022-02-26 11:53:15 +0100

    implement structopt for web-server cli flags
    for now just for debug & interface flags,
    but in future also for database related vars

--- plabayo-news-web/src/main.rs
+++ plabayo-news-web/src/main.rs
@@ -15,14 +15,34 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 use actix_web::{middleware, web, App, HttpServer};
+use anyhow::{Context, Result};
+use structopt::StructOpt;
 
 use plabayo_news_web::site::middleware as pn_middleware;
 use plabayo_news_web::site::state::AppState;
 use plabayo_news_web::site::{assets, pages};
 
+#[derive(StructOpt, Debug)]
+#[structopt(name = "plabayo-news-web")]
+struct Opt {
+    /// enable debugging features such as the logger
+    #[structopt(short, long)]
+    debug: bool,
+
+    /// interface to bind to
+    #[structopt(short, long, default_value = "127.0.0.1:8080")]
+    interface: String,
+}
+
 #[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    std::env::set_var("RUST_LOG", "actix_web=info");
+async fn main() -> Result<()> {
+    let opt = Opt::from_args();
+
+    if opt.debug {
+        std::env::set_var("RUST_LOG", "actix_web=info");
+    } else {
+        std::env::set_var("RUST_LOG", "actix_web=error");
+    }
     env_logger::init();
 
     // create app state used by all routes
@@ -42,7 +62,15 @@ async fn main() -> std::io::Result<()> {
             .service(assets::factory())
             .service(pages::factory())
     })
-    .bind("0.0.0.0:8888")?
+    .bind(&opt.interface)
+    .with_context(|| {
+        format!(
+            "bind Plabayo News HTTPServer to interface: {}",
+            opt.interface
+        )
+    })?
     .run()
-    .await
+    .await?;
+
+    Ok(())
 }
